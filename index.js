362dfd99ea25d8adbc9e85fa3634ffa6ee2888ea
sanjanabaddukonda <114835671+sanjanabaddukonda@users.noreply.github.com>
2022-10-25 17:31:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/goodbyeworld', (req, res) => {
+  res.send('goodbye World!')
+})
+
+app.listen(port,() =>{
+  console.log('Example app listening on port ${port}')
+})
